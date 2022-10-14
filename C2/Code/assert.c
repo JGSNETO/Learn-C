813d50ebb3bd90537326eae1525e33eee3a8aa43
José Neto <jgsnto.sd@gmail.com>
2022-10-13 21:14:19 -0300

    End of W1_C2: Assert and Preprocessors

---
+++ C2/Code/assert.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// #include <assert.h>
+
+// int main(){
+    
+//     int x = 7;
+//     x = 9;
+//     assert(x==7);
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+
+int main(){
+    assert(0);
+    printf("My program runs\n");
+    return 0;
+}
