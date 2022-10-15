2895ab2a724c8570af273462800a56914307bb60
José Neto <jgsnto.sd@gmail.com>
2022-10-15 10:15:03 -0300

    C2_W2&W3: Linked list, stack, queue and struct

---
+++ C2/Code/struct.c
@@ -0,0 +1,20 @@
+// Basic Example
+
+// #include <stdio.h>
+
+// struct Point{
+//     int x, y;
+// } p1; // The variable p1 is declared with 'point'
+
+// // A variable declaration like basic data types
+// struct Point
+// {
+//     int x, y;
+// };
+
+// int main(){
+
+//     struct Point p1;
+//     return 0;
+// }
+
