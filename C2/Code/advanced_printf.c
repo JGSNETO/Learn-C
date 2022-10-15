4b4ad5716c951babffb2c2b26ed300c6bcebd124
José Neto <jgsnto.sd@gmail.com>
2022-10-15 14:18:21 -0300

    End of C2_W3: Binary tree and advanced printf

---
+++ C2/Code/advanced_printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+
+    double x = .123456789;
+    printf("General printing ideas \n\n");
+    printf("X is %-12.5e and %e and %10.5f and %10d\n\n", x,x,x,x);
+    return 0;
+}
